6b0c8dbd9970ed16056c9f7b7ccb03f987a66eeb
Jeffrey Chen <jeffreychen@BNAJChenMac.local>
2023-04-07 15:57:31 -0500

    feat: review react basics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import the libs
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// get reference to div with id root
+const el = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// tell react to take control of that element
+const root = ReactDOM.createRoot(el);
+
+// render the component
+root.render(<App />);
